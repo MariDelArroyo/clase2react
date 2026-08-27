ea9f1cae6eac3a63f9f4cb9a957d09d73d604f07
MariDelArroyo <marianadelarroyo1@upb.edu>
2026-08-27 18:08:27 -0400

    sachita caquita

---
+++ tic-tac-toe/src/tictactoe.jsx
@@ -0,0 +1,23 @@
+export default function TicTacToe() {
+    return (
+        <section>
+            <table>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </table>
+        </section>
+    );
+}
